432bc6e066db218f55a3435068fa87ef4b1263eb
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2024-04-18 23:22:48 +0900

    [설정] setting을 main에 동기화 (#38)
    * Setting (#10)
    
    * [설정] ISUUE 충돌 템플릿 추가
    
    * Revert "[설정] ISSUE : 충돌 템플릿 추가"
    
    This reverts commit bfb0c69c5e78a70fd868e8b57ebf6d4b00f253bb.
    
    * [설정] 이슈 템플릿 수정
    
    * [설정] 이슈 템플릿 수정 (#11)
    
    * [설정] ISUUE 충돌 템플릿 추가
    
    * Revert "[설정] ISSUE : 충돌 템플릿 추가"
    
    This reverts commit bfb0c69c5e78a70fd868e8b57ebf6d4b00f253bb.
    
    * [설정] 이슈 템플릿 수정
    
    * [설정] 서하진 유니티 파일 업로드 (#12)
    
    * Revert "[설정] 서하진 유니티 파일 업로드 (#12)" (#15)
    
    This reverts commit 43957a34c33cf591aee436de5b19883fc6a58146.
    
    * [설정] 서하진 유니티 파일 업로드 (#17)
    
    * [설정] 서하진 유니티 파일 업로드 (#13)
    
    * [설정] 서하진 유니티 파일 업로드 (#12)
    
    * Revert "[설정] 서하진 유니티 파일 업로드 (#12)"
    
    This reverts commit 43957a34c33cf591aee436de5b19883fc6a58146.
    
    * Revert "[설정] 서하진 유니티 파일 업로드 (#13)" (#14)
    
    This reverts commit fb8d5d341bf1a442bf431d72857931b87b069575.
    
    * [설정] 서하진 유니티 파일 업로드 (#16)
    
    * [설정] 서하진 유니티 파입 업로드 (최종)
    
    * [설정] ReadME 파일 test 제거
    
    * [설정] README 꾸미기 - header
    
    header 만들기
    
    * Update README.md
    
    * [설정] README 꾸미기 - stack 배지 만들기
    
    * [설정] README 꾸미기 - 공동작업자 추가
    
    * [설정] README 꾸미기 - Wiki 추가
    
    * 2024.04.05 노준영 파일 업로드 (#19)
    
    * [설정] 오브젝트 아이템 branch,  scene 생성
    
    * [오브젝트] InteractionBasic 스크립트 생성하기
    
    * [설정] 박현 유니티 파일 재업로드
    
    유니티 파일 수정 ...ect
    
    * [설정] 이전 파일 삭제
    
    * [설정] 노준영 파일 업로드
    
    * [설정] 파일 업로드
    
    * [설정] 파일 업로드
    
    * [setting]shj file upload (#36)
    
    ---------
    
    Co-authored-by: Jun-Young Noh <119721103+junyoungnnn@users.noreply.github.com>
    Co-authored-by: ParkHyeon89 <parkhyeon0809@gmail.com>
    Co-authored-by: ParkHyeon89 <163558275+ParkHyeon89@users.noreply.github.com>
    Co-authored-by: hhchan15 <86926175+hhchan15@users.noreply.github.com>
    Co-authored-by: hhchan15 <huhchan515@pukyong.ac.kr>
    Co-authored-by: westging <109637733+westging@users.noreply.github.com>

---
+++ catTown/Assets/Junyoung/Scripts/Junyoung/Monster.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Monster : MonoBehaviour
+{
+    public LayerMask whatIsTarget; // 추적 대상 레이어
+    private Player targetEntity; // 추적 대상
+    private NavMeshAgent navMeshAgent; // 경로 계산 AI 에이전트
+    private Animator monsterAnimator; // 애니메이터 컴포넌트
+    private bool isAttacking = false; // 공격 중인지 여부
+
+    // 추적할 대상이 존재하는지 알려주는 프로퍼티
+    private bool hasTarget
+    {
+        get
+        {
+            // 추적할 대상이 존재하고, 대상이 사망하지 않았다면 true
+            if (targetEntity != null && !targetEntity.dead)
+            {
+                return true;
+            }
+
+            // 그렇지 않다면 false
+            return false;
+        }
+    }
+
+    private void Awake()
+    {
+        // 초기화
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        monsterAnimator = GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+        // 게임 오브젝트 활성화와 동시에 AI의 추적 루틴 시작
+        StartCoroutine(UpdatePath());
+    }
+
+    void Update()
+    {
+        // 추적 대상의 존재 여부에 따라 다른 애니메이션 재생
+        monsterAnimator.SetBool("HasTarget", hasTarget);
+    }
+
+    // 주기적으로 추적할 대상의 위치를 찾아 경로 갱신
+    private IEnumerator UpdatePath()
+    {
+        // 살아 있는 동안 무한 루프
+        while (true)
+        {
+            if (!hasTarget)
+            {
+                // 추적 대상이 없을 때 코드
+                Collider[] colliders = Physics.OverlapSphere(transform.position, 20f, whatIsTarget);
+
+                // 주변에 플레이어가 있는지 확인
+                for (int i = 0; i < colliders.Length; i++)
+                {
+                    Player player = colliders[i].GetComponent<Player>();
+                    if (player != null && !player.dead)
+                    {
+                        targetEntity = player;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                // 추적 중인 경우
+                if (Vector3.Distance(transform.position, targetEntity.transform.position) <= 5f && !isAttacking)
+                {
+                    // 플레이어가 일정 범위 내에 있으면 공격
+                    monsterAnimator.SetTrigger("Hit");
+                }
+                else
+                {
+                    // 일정 범위 내에 플레이어가 없으면 추적 계속
+                    navMeshAgent.isStopped = false;
+                    navMeshAgent.SetDestination(targetEntity.transform.position);
+                }
+            }
+
+            // 0.25초 주기로 처리 반복
+            yield return new WaitForSeconds(0.25f);
+        }
+    }
+}
